c8888897b56d4c76a8825e111864cd9e129ee894
4ayyappadasks <ayyappanks141@gmail.com>
2023-04-04 01:55:59 +0530

    commit

--- bin/for.dart
+++ bin/for.dart
@@ -4,7 +4,7 @@ void main()
   print("enter the value");
   int a = int.parse(stdin.readLineSync()!);
 
-  for(int n=1 ; n<= 10 ; n++)
+  for(int n=1 ; n<= 25 ; n++)
     {
     print("$n * $a = ${n*a}");
     }
